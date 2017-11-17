0ede83a834d0397549c15aff4a203ef97159f96d
lilang <11170370113@qq.com>
2017-11-17 11:26:21 +0800

    时间帮助类更新

--- utils/TimeUtils.java
+++ utils/TimeUtils.java
@@ -8,8 +8,74 @@ import java.util.Date;
  * Time: 2017/11/13.
  */
 public class TimeUtils {
-    public static String getCurrentTime(){
+    /**
+     *
+     * @return 数据格式:2014-12-19
+     */
+    public static String getCurDate(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(new Date());
+    }
+    public static List<String> getBeenByDays(String start_time, String end_time){
         SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
-        return  format.format(new Date());
+        Date sdate=null;
+        Date eDate=null;
+        try {
+            sdate=format.parse(start_time);
+            eDate=format.parse(end_time);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        Calendar c = Calendar.getInstance();
+        List<String> list=new ArrayList<String>();
+        while (sdate.getTime()<=eDate.getTime()) {
+            list.add(format.format(sdate));
+            c.setTime(sdate);
+            c.add(Calendar.DATE, 1); // 日期加1天
+            sdate = c.getTime();
+        }
+        return list;
+    }
+
+    /**
+     *
+     * @return 数据格式:2014-12-19
+     */
+
+    public static String getCurDateTime(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        return dateFormat.format(new Date());
+    }
+    /*
+     * 将时间转换为时间戳
+     * 时间格式为2017-01-01 11:12:13 否则抛异常
+     */
+    public static String dateToStamp(String s) throws ParseException{
+        String res;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = simpleDateFormat.parse(s);
+        long ts = date.getTime();
+        res = String.valueOf(ts);
+        return res;
+    }
+
+    /**
+     * 获取当前时间的时间戳
+     * @return
+     */
+    public static String getCurDateStamp(){
+       return String.valueOf(System.currentTimeMillis());
+    }
+    /*
+     * 将时间戳转换为时间
+     */
+    public static String stampToDate(String s){
+        String res;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        long lt = new Long(s);
+        Date date = new Date(lt);
+        res = simpleDateFormat.format(date);
+        return res;
     }
 }
